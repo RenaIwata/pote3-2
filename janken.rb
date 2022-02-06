6deb0031fcb48b6310cbfe4cdc8cc2f68b7875d0
RenaIwata <puqpuqchan_07@icloud.com>
2022-02-06 17:13:35 +0900

    あっち向いてほいできた

--- janken.rb
+++ janken.rb
@@ -1,4 +1,4 @@
-    puts "じゃんけん..."
+       puts "じゃんけん..."
 def janken
     puts "0(グー) 1(チョキ) 2(パー) 3(戦わない)"
     choice = gets.chomp.to_i
@@ -13,33 +13,43 @@ def janken
         puts "あいこで..."
         return true
     elsif choice == 0 && you == 1 || choice == 1 && you == 2 || choice == 2 && you == 0 
-        @result = "win"
-        return false
-    else    
-        puts "もう一回！"
-        return true
+        puts "あっちむいて..."
+        puts "0(上) 1(下) 2(右) 3(左)"
+        me = gets.chomp.to_i
+        finger = ["上", "下", "右", "左"]
+        program = rand(4)
+        puts "ホイ！"
+        puts "-----------------"
+        puts "あなた: #{finger[me]}"
+        puts "相手: #{finger[program]}"
+        if me == program
+            puts "あなたの勝ち！"
+            return false
+        else me != program
+            puts "もう一度じゃんけんから！"
+            return true
+        end
+    else 
+        puts "負けたので顔を向ける方向を決めてください"  
+        puts "あっちむいて..."
+        puts "0(上) 1(下) 2(右) 3(左)"
+        me = gets.chomp.to_i
+        finger = ["上", "下", "右", "左"]
+        program = rand(4)
+        puts "ホイ！"
+        puts "-----------------"
+        puts "あなた: #{finger[me]}"
+        puts "相手: #{finger[program]}"
+        if me == program
+            puts "あなたの負け！"
+            return false
+        else me != program
+            puts "もう一度じゃんけんから！"
+            return true
+        end
     end
 end
 again_game = true
 while again_game
     again_game = janken
 end
-
-if @result == "win"
-    # def again_game
-    puts "あっちむいて..."
-    puts "0(上) 1(下) 2(右) 3(左)"
-    next_choice = gets.chomp.to_i
-    hand = ["上", "下", "右", "左"]
-    you = rand(4)
-    puts "ホイ！"
-    puts "-----------------"
-    puts "あなた: #{hand[next_choice]}"
-    puts "相手: #{hand[you]}"
-    # end
-end
-    if next_choice == you
-        puts "あなたの勝ち！"
-    else    
-        puts "あなたの負け！"
-    end
